eb430deb2fac3c18fa65d192a9a792ccb0713dc8
Billy Lowry <billyn.lowry@gmail.com>
2023-03-22 22:18:02 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Less than or equal to 400 ft 
+  if (feet <= 400){
+    return "This one is on me!"
+  // Greater than 400 ft & less than 2000 ft
+  } else if (feet < 2000){
+    return "That will be twenty bucks."
+  // Greater than or equal to 2500 ft
+  } else if (feet <= 2500){
+    return "I will gladly take your thirty bucks."
+  // Between 2000 and 2500 ft 
+  }  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(theBag){
+  switch(theBag) {
+    case 'generous' :
+    return 'Thank you so much.' ;
+    case 'not as generous' :
+    return 'Thank you.'  ;
+    default:
+      return 'Bye.' ;
+  }
 }
